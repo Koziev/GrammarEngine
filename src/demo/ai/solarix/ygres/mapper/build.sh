c0c5b1d9716e026025cc910144d6c1993dd2d9c1
Koziev <inkoziev@gmail.com>
2017-10-19 13:04:27 +0300

    make and CMake scripts for decompiler

--- src/demo/ai/solarix/ygres/mapper/build.sh
+++ src/demo/ai/solarix/ygres/mapper/build.sh
@@ -1,22 +1,7 @@
-if [ "$OSTYPE" == "darwin10.0" ]
-then
-
- make -f makefile.darwin64
-
-else
-
- if [ -d "/tmp/ymap" ];
- then echo ""
- else mkdir /tmp/ymap
- fi
- 
- if [ "$HOSTTYPE" == "x86_64" ]
- then
-  make -f makefile.linux64
-  cp decompiler ../../../../../exe64
- else
-  make -f makefile.linux
-  cp decompiler ../../../../../exe
- fi
-
+if [ -d "/tmp/ymap" ];
+then echo ""
+else mkdir /tmp/ymap
 fi
+ 
+make -f makefile.linux64
+cp decompiler ../../../../../exe64
