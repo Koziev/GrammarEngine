e6f37aa2f5f8e54ef1c27038961b02e712629de1
inkoziev <mentalcomputing@gmail.com>
2017-12-12 17:53:33 +0400

    Добавлены скрипты запуска интерактивного отладчика парсера и part-of-speech tagger'а в .../scripts/syntax

---
+++ src/scripts/syntax/console-morphology.cmd
@@ -0,0 +1,3 @@
+rem SYNTAX.EXE manual page: http://www.solarix.ru/for_developers/exercise/syntax-analysis.shtml
+
+..\..\exe64\Syntax.exe -dictdir ..\..\bin-windows64 -lazy_lexicon -disallow_incomplete -morphology -language autodetect -maxalt 20
---
+++ src/scripts/syntax/console-syntax.cmd
@@ -0,0 +1,5 @@
+rem Запуск консольного отладчика синтаксического парсера
+rem SYNTAX.EXE manual page: http://www.solarix.ru/for_developers/exercise/syntax-analysis.shtml
+
+..\..\exe64\Syntax.exe -dictdir ..\..\bin-windows64 -lazy_lexicon -disallow_incomplete -syntax -maxalt 30
+
